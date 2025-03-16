3cf9236b009ccb930c758e576842f9af77d984f5
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2025-03-16 01:02:29 -0400

    ocean simulation - copy from gist commit

---
+++ main.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python
+
+import argparse
+import io
+import multiprocessing
+import os
+import random
+import sys
+import threading
+import time
+import traceback
+from typing import List
+from typing import NoReturn
+from typing import Tuple
+
+from ocean import Ocean
+
+
+class SimText:
+    @staticmethod
+    def paint(ocean: Ocean, file=sys.stdout):
+        print("\033[0;0H\n", file=file)
+        print(ocean, file=file)
+
+    @staticmethod
+    def random_ocean(width: int, height: int) -> Ocean:
+        x, y, r, offset = 0, 0, random.Random(), 78887
+        ocean = Ocean(width, height)
+
+        for xx in range(width):
+            x = (x + offset) % width
+            if x & 8 == 0:
+                for yy in range(height):
+                    y = (y + offset) % height
+                    if y & 8 == 0:
+                        rand = r.random()
+                        if rand < 0.5:
+                            ocean.add_fish(x, y)
+                        elif rand >= 0.9:
+                            ocean.add_shark(x, y)
+
+        return ocean
+
+
+# def simulate_ocean(ocean: Ocean, starve_time: int, frames: int, queue: multiprocessing.Queue, signal: threading.Event):
+def simulate_ocean(
+    ocean: Ocean,
+    starve_time: int,
+    frames: int,
+    kill_repeat: bool,
+    queue: multiprocessing.Queue,
+    sigInt: multiprocessing.Event,
+) -> None:
+    """
+    Simulate the ocean
+    """
+    # top & bottom
+    top_bot = "\n     " + "!" * (2 * ocean.width - 8)
+
+    def mid(word: str) -> str:
+        s1 = s2 = (2 * ocean.width - 16 - len(word)) // 2
+        s2 += len(word) % 2
+        return "\n     !!!!" + " " * s1 + word.upper() + " " * s2 + "!!!!"
+
+    def dead_str() -> str:
+        return top_bot + mid("dead") + top_bot + "\n"
+
+    def exc_str() -> str:
+        return f"{top_bot}{mid('exception')}{top_bot}\n"
+
+    def halt_str() -> str:
+        return f"{top_bot}{mid('halted')}{top_bot}\n"
+
+    def kill_str() -> str:
+        return f"{top_bot}{mid('killed')}{top_bot}\n"
+
+    def repeat_str() -> str:
+        return f"{top_bot}{mid('repeat')}{top_bot}\n"
+
+    # hash set
+    seen = {hash(ocean)}
+
+    # track if we're going to run a limited number of frames
+    counter = 0
+
+    def forever():
+        """
+        simple generator that tells the loop to keep going or to halt
+        """
+        nonlocal counter
+        if frames > 0:
+            counter += 1
+            if counter == frames:
+                return
+        yield True
+
+    try:
+        while next(forever()):
+            # print(
+            #     f"\033[50;0H {counter} frames calculated",
+            #     end="",
+            #     file=sys.stdout,
+            # )
+            # counter += 1
+            queue.put((ocean, ""))
+
+            ocean = ocean.time_step(starve_time)
+            # check for exit conditions
+            if ocean.is_dead:
+                queue.put((ocean, dead_str()))
+                break
+            if sigInt.is_set():
+                queue.put((ocean, kill_str()))
+                break
+            if kill_repeat:
+                if (h := hash(ocean)) in seen:
+                    queue.put((ocean, repeat_str()))
+                    break
+                else:
+                    seen.add(h)
+    except KeyboardInterrupt:
+        queue.put((ocean, kill_str()))
+    except:
+        # record the exception into a buffer
+        exc_buffer = io.StringIO()
+        traceback.print_exc(file=exc_buffer)
+        # return the halted ocean with the exc/tb
+        queue.put((ocean, exc_str() + "\n" + exc_buffer.getvalue()))
+    else:
+        # natural exit
+        # queue.put((ocean, halt_str()))
+        queue.put((None, None))
+        # signal to consumers that we are done
+        queue.close()
+        sigInt.clear()
+
+
+# def run_ocean(ocean: Ocean, starve_time: int, fps: int=24, frames: int=-1) -> Tuple[multiprocessing.Queue, threading.Event]:
+def run_ocean(
+    ocean: Ocean,
+    sigInt: multiprocessing.Event,
+    *,
+    starve_time: int = 3,
+    fps: int = 24,
+    frames: int = -1,
+    kill_repeat: bool = False,
+) -> Tuple[multiprocessing.Process, multiprocessing.Queue]:
+    """
+    Runs the ocean in a separate process
+    """
+    queue = multiprocessing.Queue(maxsize=int(fps * 1.5))
+    # queue = multiprocessing.Queue()
+    # signal = threading.Event()
+    # signal.set()
+
+    # runner = threading.Thread(target=simulate_ocean, args=(ocean, starve_time, frames, queue, sigInt,))
+    runner = multiprocessing.Process(
+        target=simulate_ocean,
+        args=(
+            ocean,
+            starve_time,
+            frames,
+            kill_repeat,
+            queue,
+            sigInt,
+        ),
+    )
+    runner.daemon = True
+    # runner.setDaemon(True)  # for threading
+    runner.start()
+
+    # return the running process and the event queue
+    return runner, queue
+
+
+def args_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser("SimText command line arguments")
+
+    parser.add_argument(
+        "-x",
+        "--width",
+        default=40,
+        type=int,
+        metavar="WIDTH",
+        help="The width of the ocean",
+    )
+    parser.add_argument(
+        "-y",
+        "--height",
+        default=40,
+        type=int,
+        metavar="HEIGHT",
+        help="The height of the ocean",
+    )
+    parser.add_argument(
+        "-s",
+        "--starve_time",
+        default=3,
+        type=int,
+        metavar="STARVE_TIME",
+        help="The starve time for the sharks",
+    )
+    parser.add_argument(
+        "-f",
+        "--fps",
+        default=24,
+        type=int,
+        metavar="FPS",
+        help="Frames per second to run the simulation at",
+    )
+    parser.add_argument(
+        "-r",
+        "--frames",
+        default=-1,
+        type=int,
+        metavar="FRAMES",
+        help="The max number of frames to run",
+    )
+    parser.add_argument(
+        "-k",
+        "--kill-repeat",
+        action="store_true",
+        help="Flag determines if we should kill the ocean on a repetition",
+    )
+
+    return parser
+
+
+def main(sigInt: multiprocessing.Event) -> NoReturn:
+    # # parsed args as vars
+    # args = vars(args_parser().parse_args())
+    #
+    # # defaults args
+    # width, height, starve_time, fps, frames, kill_repeat = (
+    #     args["width"],
+    #     args["height"],
+    #     args["starve_time"],
+    #     args["fps"],
+    #     args["frames"],
+    #     args["kill_repeat"],
+    # )
+    #
+    # parse our CLI arguments
+    kwargs = vars(args_parser().parse_args())
+
+    width, height = kwargs.pop("width"), kwargs.pop("height")
+    fps, starve_time = kwargs["fps"], kwargs["starve_time"]
+
+    # initialize a random ocean
+    ocean = SimText.random_ocean(width, height)
+    SimText.paint(ocean)
+
+    # start the ocean simulation thread
+    proc, queue = run_ocean(ocean, sigInt, **kwargs)
+
+    # frame count
+    frame = -1
+
+    # sleep factor fudges the sleep duration to account for overhead
+    # sleep_factor = 1.0 + (2.0 / 30.0)  # 1.0666...
+    sleep_factor = 1.06775
+
+    # sleep duration - fudged to account for overhead
+    sleep = 1.0 / (fps**sleep_factor)
+
+    input("press enter to start: ")
+
+    # track our start time
+    start = time.time()
+
+    # play the ocean
+    while proc.is_alive() or not queue.empty():
+        try:
+            ocean, msg = queue.get()
+            if ocean is None:
+                break
+
+            # increment frame
+            frame += 1
+
+            # print the ocean
+            print("\033[0;0H\n")
+            print(str(ocean) + msg)
+
+            # print current frame info
+            elapsed = time.time() - start
+            print(
+                "[",
+                f"{elapsed:.1f}s",
+                "]",
+                "frame:",
+                frame,
+                "(",
+                width,
+                height,
+                starve_time,
+                fps,
+                ")",
+                "fps:",
+                f"{frame/elapsed:.1f}",
+                "[",
+                hash(ocean),
+                "]",
+                end="",
+            )
+
+            # sleep for a bit
+            time.sleep(sleep)
+        except KeyboardInterrupt:
+            sigInt.set()
+            raise
+
+
+if __name__ == "__main__":
+    interrupt = False
+    # process signal
+    sigInt = multiprocessing.Event()
+
+    try:
+        # main(sigInt, *sys.argv[1:])
+        main(sigInt)
+    except KeyboardInterrupt:
+        interrupt = True
+        sigInt.wait()  # wait for the signal to be cleared
+        sys.stdout.flush()
+        print()
+        # raise
+    except:  # pylint: disable=bare-except
+        # os.system('clear')
+        raise
+    finally:
+        if interrupt:
+            input("\n^C: press enter to clear: ")
+            os.system("clear")
---
+++ ocean.py
@@ -0,0 +1,210 @@
+from collections import Counter
+from collections import defaultdict
+from enum import Enum
+from enum import auto
+from io import StringIO
+from itertools import product
+from random import Random
+from typing import Any
+from typing import Callable
+from typing import DefaultDict
+from typing import Dict
+from typing import Iterable
+from typing import List
+from typing import Tuple
+
+
+class Occupant(Enum):
+    EMPTY = auto()
+    SHARK = auto()
+    FISH = auto()
+
+
+class Cell:
+    def __init__(self, occupant: Occupant, feeding: int = 0):
+        self.occupant = occupant
+        self.feeding = feeding
+
+    def __str__(self) -> str:
+        return f"Cell[{str(self.occupant)}, {str(self.feeding)}]"
+
+    def __repr__(self) -> str:
+        return f"Cell({str(self.occupant)}, feeding={repr(self.feeding)})"
+
+
+EMPTY_CELL = Cell(Occupant.EMPTY)
+
+
+class DefaultNoSetDict(dict):
+    def __init__(self, default_factory: Callable[[], Any], *args, **kwargs):
+        # initialize the dict
+        super().__init__(*args, **kwargs)
+        # define our default_factory
+        self.default_factory = default_factory
+
+    def get(self, key: Any, default: Any = None) -> Any:
+        super().get(key, default or self.default_factory())
+
+    def __getitem__(self, key: Any) -> Any:
+        if key not in self:
+            return self.default_factory()
+
+        return super().__getitem__(key)
+
+
+def empty_cell_factory() -> Cell:
+    return EMPTY_CELL
+
+
+class Ocean:
+    def __init__(self, width: int, height: int, _buffer: StringIO = None):
+        self._width: int = width
+        self._height: int = height
+        # self._ocean: DefaultDict[Cell] = defaultdict(lambda: EMPTY_CELL)
+        self._ocean: DefaultNoSetDict = DefaultNoSetDict(empty_cell_factory)
+        self._rand = Random()
+        # text buffer
+        self._buffer = _buffer or StringIO()
+        self.__str = None
+
+    def __len__(self) -> int:
+        return len(self._ocean)
+
+    def __setitem__(self, coords: Tuple[int, int], cell: Cell):
+        self._ocean[self._coords(*coords)] = cell
+
+    def __getitem__(self, coords: Tuple[int, int]) -> Cell:
+        return self._ocean[self._coords(*coords)]
+
+    def __str__(self) -> str:
+        if self.__str:
+            return self.__str
+
+        self._buffer.seek(0)
+
+        for y in range(self.height):
+            self._buffer.write(" ")
+            for x in range(self.width):
+                # fetch the occupant
+                occupant = self[(x, y)].occupant
+                if occupant == Occupant.SHARK:
+                    self._buffer.write("🦈")
+                elif occupant == Occupant.FISH:
+                    self._buffer.write("🐠")
+                else:
+                    # self._buffer.write('🌊')
+                    self._buffer.write("📘")
+            self._buffer.write("\n")
+
+        self.__str = self._buffer.getvalue()
+        return self.__str
+
+    def __hash__(self) -> str:
+        return hash(self.__str__())
+
+    def random(self) -> float:
+        return self._rand.random()
+
+    def randint(self, lo: int, hi: int) -> int:
+        return self._rand.randint(lo, hi)
+
+    @property
+    def width(self) -> int:
+        return self._width
+
+    @property
+    def height(self) -> int:
+        return self._height
+
+    def _coords(self, x: int, y: int) -> Tuple[int, int]:
+        return x % self.width, y % self.height
+
+    def _add(self, x: int, y: int, occupant: Occupant, feeding: int = 0) -> None:
+        x, y = self._coords(x, y)
+        if self._ocean[(x, y)] == EMPTY_CELL:
+            self._ocean[(x, y)] = Cell(occupant, feeding)
+
+    def add_fish(self, x: int, y: int) -> None:
+        self._add(x, y, Occupant.FISH)
+
+    def add_shark(self, x: int, y: int, feeding: int = 0) -> None:
+        self._add(x, y, Occupant.SHARK, feeding)
+
+    def cell_contents(self, x: int, y: int) -> Occupant:
+        return self[(x, y)].occupant
+
+    @property
+    def is_dead(self) -> bool:
+        # return len(self) == 0
+        return False
+
+    def shark_feeding(self, x: int, y: int) -> int:
+        pass
+
+    def time_step(self, starve_time: int) -> "Ocean":
+        # new_ocean = Ocean(self.width, self.height, self._buffer)  # share a buffer???
+        new_ocean = Ocean(self.width, self.height)
+
+        for x, y in product(range(self.width), range(self.height)):
+            # # life finds a way
+            # cell = self._spontaneous_life_emergence(x, y, starve_time)
+            # if cell:
+            #     new_ocean._ocean[(x, y)] = cell
+            #     continue
+
+            # apply rules
+            cell = self._apply_rules(x, y, starve_time)
+            if cell is not EMPTY_CELL:
+                new_ocean._ocean[(x, y)] = cell
+
+        return new_ocean
+
+    # def _spontaneous_life_emergence(self, x: int, y: int, starve_time: int) -> Cell:
+    #     if self._ocean[(x, y)] is not EMPTY_CELL:
+    #         return EMPTY_CELL
+    #     # 2% change
+    #     if self.random() <= 0.02:
+    #         return EMPTY_CELL
+    #     # 20:80 SHARK:FISH
+    #     if self.randint() < 2:
+    #         return Cell(Occupant.SHARK, starve_time)
+    #     return Cell(Occupant.FISH)
+
+    def _apply_rules(self, x: int, y: int, starve_time: int) -> Cell:
+        cell = self._ocean[(x, y)]  # current cell
+
+        counts = Ocean.counts(self._neighbours(x, y))
+        fishes, sharks = (
+            counts.get(Occupant.FISH, 0),
+            counts.get(Occupant.SHARK, 0),
+        )
+
+        if cell.occupant == Occupant.SHARK:
+            feeding = starve_time if fishes else cell.feeding - 1
+            if feeding:
+                return Cell(Occupant.SHARK, feeding)
+
+        if cell.occupant == Occupant.FISH:
+            if sharks > 1:
+                return Cell(Occupant.SHARK, starve_time)
+            return Cell(Occupant.FISH)
+
+        if sharks <= 1 and fishes > 1:
+            return Cell(Occupant.FISH)
+
+        if sharks > 1 and fishes > 1:
+            return Cell(Occupant.SHARK, starve_time)
+
+        return EMPTY_CELL
+
+    def _neighbours(self, x: int, y: int) -> List[Cell]:
+        return [
+            self[(x + i, y + j)]
+            for i in range(-1, 1 + 1)
+            for j in range(-1, 1 + 1)
+            if (i, j) != (0, 0)
+        ]
+
+    @staticmethod
+    def counts(cells: Iterable[Cell]) -> Dict[Occupant, int]:
+        return Counter(c.occupant for c in cells)
